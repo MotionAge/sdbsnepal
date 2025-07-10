aa16d5805394d96669f675f668de8a3ea19eb29e
AshimKoirala <ashimkoirala01@gmail.com>
2025-07-10 17:44:14 +0545

    url

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,10 +1,12 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 import type { Database } from "./database.types"
+import { Resend } from "resend"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const createServerClient = () => {
   const cookieStore = cookies()
